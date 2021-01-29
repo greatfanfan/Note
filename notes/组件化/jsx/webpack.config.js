55e69e3adf97b4e1279171555f8aa61d2d5812ec
greatfanfan <fanfan_nancy@163.com>
2021-01-29 20:27:42 +0800

    update

---
+++ notes/组件化/jsx/webpack.config.js
@@ -0,0 +1,21 @@
+const { LibManifestPlugin } = require("webpack");
+
+// node的模块
+module.exports = {
+  entry: "./main.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-react-jsx"]
+          }
+        }
+      }
+    ]
+  },
+  mode: "development"
+}
